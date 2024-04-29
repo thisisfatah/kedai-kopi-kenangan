765d068bc4e3191063d60729b9902fe2129fb1df
COMPUTER\anyon <fatah>
2024-04-29 16:44:06 +0700

    updated shop cart

--- js/script.js
+++ js/script.js
@@ -1,13 +1,63 @@
+// 
+// 
 const navbarNav = document.querySelector('.navbar-nav');
 
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
-const hamburger = document.querySelector('#hamburger-menu');
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) =>{
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+};
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
-})
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
+});
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach(element => {
+    element.onclick = (e) =>{
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+window.onclick = (e) =>{
+    if(e.target == itemDetailModal){
+    itemDetailModal.style.display = 'none';
+    }
+}
